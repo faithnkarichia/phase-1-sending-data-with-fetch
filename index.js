63f05af09a10bf959ebaa44df24d23c2df796d5a
Faith <kashfaith77@gmail.com>
2025-03-29 17:54:08 +0300

    Add Post nethod

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Add your code here
+function submitData(name, email) {
+  return fetch("http://localhost:3000/users", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify({ name, email }),
+  })
+    .then(function (response){
+      return response.json()})  
+    .then(function (data) {
+      let body = document.body;
+     body.innerHTML=data.id
+    })
+    .catch(function (error)  {
+      let body = document.body;
+      
+     
+      body.innerHTML = error.message; 
+     
+    });
+}
+
+
+submitData("steve", "steve@steve.com");
